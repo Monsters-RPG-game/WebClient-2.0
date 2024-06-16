4cd7d604e1931a8d54432d5f65a3074f5ba0b09f
DominikDO98 <dominiklas13@wp.pl>
2024-06-16 14:05:55 +0200

    Feat: Basic folder and components structure for homepage

---
+++ src/components/home/Welcome.tsx
@@ -0,0 +1,7 @@
+export const Welcome = () => {
+    return <>
+    <div>Welcome, Player!</div>
+    <div>some greeting for user</div>
+    <div>basic discritpion of the game</div>
+    </>
+}
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,8 @@
+export const Homepage = () => {
+    return <>
+    <h1>Hello, World!</h1>
+   <div>some basic info</div> 
+    <div>login</div>
+    <div>more game info</div>
+    </>
+}
